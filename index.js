bc7db0ae43c3dfc6b019283d811cd435f1ee62a6
ndineshd <ndineshb@northladder.com>
2023-09-05 12:59:39 +0530

    script:Add js file with function script

---
+++ index.js
@@ -0,0 +1,9 @@
+function myTime () {
+    document.getElementById('time').innerHTML = Date();
+}
+function myBulbOn() {
+    document.getElementById('myBulb').src='images/bulbon.gif';
+}
+function myBulbOff() {
+    document.getElementById('myBulb').src='images/bulboff.gif';
+}
